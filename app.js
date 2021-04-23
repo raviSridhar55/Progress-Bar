b57dba533d44a8079f8cc021d68adddbde3974c1
raviSridhar55 <61363065+raviSridhar55@users.noreply.github.com>
2021-04-23 14:34:33 +0530

    Progress Bar

---
+++ app.js
@@ -0,0 +1,21 @@
+var design = anime({
+  targets: '.status1',
+  width: '90%', // -> from '28px' to '100%',
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: false,
+});
+var design = anime({
+  targets: '.status2',
+  width: '80%', // -> from '28px' to '100%',
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: false,
+});
+var design = anime({
+  targets: '.status3',
+  width: '70%', // -> from '28px' to '100%',
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: false,
+});
